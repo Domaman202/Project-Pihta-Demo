af7276aba465315729687636c7b8bc51f9e4c5f6
DomamaN202 <vip.domaman@mail.ru>
2023-12-13 16:00:57 +0700

    =)

--- src/build.gradle.kts
+++ src/build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 
 dependencies {
     implementation("ru.DmN.siberia:Project-Siberia:1.6.2")
-    implementation("ru.DmN.pht:Project-Pihta:1.4.1")
+    implementation("ru.DmN.pht:Project-Pihta:1.5.0")
     implementation(files("Lazurite-2.7.0.jar"))
     implementation("ru.DmN.phtx:Project-PLS:1.0.0")
 }
